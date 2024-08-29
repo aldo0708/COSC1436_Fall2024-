70b1e63e3e67e40e86f646d0536032169ff5923f
aldo0708 <aldoponce8701@gmail.com>
2024-08-29 00:32:05 +0000

    #Helloworld

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+public class Helloworld {//"public class" means that your creating a class to create objects. "Helloworld" is the name of the class. The curly braces ("{}") is the start of the class.
+    public static void main(String[] args) {// "public" means that the method can be accesed from outside of the class. "static" means that you dont need to create an object of "Helloworld". "Void" Doesn't return any value."main" This is the name of the method that java looks for to start runnning your program. "(String[] args)" This is a way to pass information into the main method. "String"refers to text. "[]" shows the list of words. "args" is the lable for the list. finally the curly brace to show the start of the main method.
+        System.out.println("Aldo");// "System" is the blueprint for the beggining of the code. The "period" is what links the command together."out" is to send out text to the screen. "println()" is a command that tells the program to show the text on screen.  The use of paranthese is used to give an argument to the method. Finally the use of the semicolon is to end of the code
+        System.out.println("Tamles");// Rinse and Repeat 
+        System.out.println("Tyler The Creator");
+        System.out.println("Doing the Dishes");//
+        System.out.println("Hanging out with my GF");//
+    }// This curly brace is to show the end of the method
+} // This curly brace is to show the end of the class
+/* 
+*The main() method is the program that java uses to run code. 
+*Java uses these tools so we can quickly and easly display meassges without doing all the set up our sleves.
+*The use of comments are to show what the line of code needs revising or changed especially when a sematic error is present.
+*/
